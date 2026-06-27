008afddbd0f4123477a6350c0c521a20358b21dd
gordillo82 <gordillojorquesangel@gmail.com>
2026-06-27 12:38:44 +0200

    Restructurar bebidas con subcategorías y precios de copa/botella para vinos

--- datos.js
+++ datos.js
@@ -244,134 +244,144 @@ const PRODUCTOS = {
     }
   ],
   
-  bebidas: [
-    {
-      nombre: "Rioja Crianza",
-      descripcion: "Vino tinto.",
-      precio: "3,80",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Ribera del Duero Roble",
-      descripcion: "Vino tinto.",
-      precio: "4,20",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Rioja Reserva",
-      descripcion: "Vino tinto.",
-      precio: "5,50",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Priorat",
-      descripcion: "Vino tinto.",
-      precio: "6,00",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Verdejo",
-      descripcion: "Vino blanco.",
-      precio: "3,80",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Albariño",
-      descripcion: "Vino blanco.",
-      precio: "4,50",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Sauvignon Blanc",
-      descripcion: "Vino blanco.",
-      precio: "4,20",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Chardonnay",
-      descripcion: "Vino blanco.",
-      precio: "4,80",
-      alergenos: "SU",
-      por_unidad: true
-    },
-    {
-      nombre: "Mahou 5 Estrellas",
-      descripcion: "Cerveza.",
-      precio: "2,80",
-      alergenos: "G",
-      por_unidad: false
-    },
-    {
-      nombre: "Alhambra Reserva 1925",
-      descripcion: "Cerveza.",
-      precio: "3,40",
-      alergenos: "G",
-      por_unidad: false
-    },
-    {
-      nombre: "IPA Artesana",
-      descripcion: "Cerveza.",
-      precio: "4,20",
-      alergenos: "G",
-      por_unidad: false
-    },
-    {
-      nombre: "Mahou Sin Alcohol",
-      descripcion: "Cerveza.",
-      precio: "2,70",
-      alergenos: "G",
-      por_unidad: false
-    },
-    {
-      nombre: "Coca-Cola",
-      descripcion: "Refresco.",
-      precio: "2,60",
-      alergenos: "",
-      por_unidad: false
-    },
-    {
-      nombre: "Coca-Cola Zero",
-      descripcion: "Refresco.",
-      precio: "2,60",
-      alergenos: "",
-      por_unidad: false
-    },
-    {
-      nombre: "Fanta Naranja",
-      descripcion: "Refresco.",
-      precio: "2,60",
-      alergenos: "",
-      por_unidad: false
-    },
-    {
-      nombre: "Aquarius Limón",
-      descripcion: "Refresco.",
-      precio: "2,60",
-      alergenos: "",
-      por_unidad: false
-    },
-    {
-      nombre: "Agua mineral 50 cl",
-      descripcion: "Botella.",
-      precio: "1,80",
-      alergenos: "",
-      por_unidad: false
-    },
-    {
-      nombre: "Agua con gas",
-      descripcion: "Botella.",
-      precio: "2,20",
-      alergenos: "",
-      por_unidad: false
-    }
-  ]
+  bebidas: {
+    tintos: [
+      {
+        nombre: "Rioja Crianza",
+        descripcion: "Vino tinto.",
+        precio_copa: "3,80",
+        precio_botella: "18,00",
+        alergenos: "SU"
+      },
+      {
+        nombre: "Ribera del Duero Roble",
+        descripcion: "Vino tinto.",
+        precio_copa: "4,20",
+        precio_botella: "21,00",
+        alergenos: "SU"
+      },
+      {
+        nombre: "Rioja Reserva",
+        descripcion: "Vino tinto.",
+        precio_copa: "5,50",
+        precio_botella: "28,00",
+        alergenos: "SU"
+      },
+      {
+        nombre: "Priorat",
+        descripcion: "Vino tinto.",
+        precio_copa: "6,00",
+        precio_botella: "32,00",
+        alergenos: "SU"
+      }
+    ],
+    blancos: [
+      {
+        nombre: "Verdejo",
+        descripcion: "Vino blanco.",
+        precio_copa: "3,80",
+        precio_botella: "18,00",
+        alergenos: "SU"
+      },
+      {
+        nombre: "Albariño",
+        descripcion: "Vino blanco.",
+        precio_copa: "4,50",
+        precio_botella: "24,00",
+        alergenos: "SU"
+      },
+      {
+        nombre: "Sauvignon Blanc",
+        descripcion: "Vino blanco.",
+        precio_copa: "4,20",
+        precio_botella: "22,00",
+        alergenos: "SU"
+      },
+      {
+        nombre: "Chardonnay",
+        descripcion: "Vino blanco.",
+        precio_copa: "4,80",
+        precio_botella: "25,00",
+        alergenos: "SU"
+      }
+    ],
+    cervezas: [
+      {
+        nombre: "Mahou 5 Estrellas",
+        descripcion: "Cerveza.",
+        precio: "2,80",
+        alergenos: "G",
+        por_unidad: false
+      },
+      {
+        nombre: "Alhambra Reserva 1925",
+        descripcion: "Cerveza.",
+        precio: "3,40",
+        alergenos: "G",
+        por_unidad: false
+      },
+      {
+        nombre: "IPA Artesana",
+        descripcion: "Cerveza.",
+        precio: "4,20",
+        alergenos: "G",
+        por_unidad: false
+      },
+      {
+        nombre: "Mahou Sin Alcohol",
+        descripcion: "Cerveza.",
+        precio: "2,70",
+        alergenos: "G",
+        por_unidad: false
+      }
+    ],
+    refrescos: [
+      {
+        nombre: "Coca-Cola",
+        descripcion: "Refresco.",
+        precio: "2,60",
+        alergenos: "",
+        por_unidad: false
+      },
+      {
+        nombre: "Coca-Cola Zero",
+        descripcion: "Refresco.",
+        precio: "2,60",
+        alergenos: "",
+        por_unidad: false
+      },
+      {
+        nombre: "Fanta Naranja",
+        descripcion: "Refresco.",
+        precio: "2,60",
+        alergenos: "",
+        por_unidad: false
+      },
+      {
+        nombre: "Aquarius Limón",
+        descripcion: "Refresco.",
+        precio: "2,60",
+        alergenos: "",
+        por_unidad: false
+      }
+    ],
+    aguas: [
+      {
+        nombre: "Agua mineral 50 cl",
+        descripcion: "Botella.",
+        precio: "1,80",
+        alergenos: "",
+        por_unidad: false
+      },
+      {
+        nombre: "Agua con gas",
+        descripcion: "Botella.",
+        precio: "2,20",
+        alergenos: "",
+        por_unidad: false
+      }
+    ]
+  }
 };
 
 // CÓDIGOS DE ALÉRGENOS
